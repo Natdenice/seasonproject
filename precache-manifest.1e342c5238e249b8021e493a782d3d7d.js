4b1bc6d4480b60aa4e248642ed4b75f1073d1be3
Natdenice <clad1987@hotmail.com>
2019-07-27 11:38:44 +0200

    Updates

---
+++ precache-manifest.1e342c5238e249b8021e493a782d3d7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a630fd91a174c5c983fd07621ef6c185",
+    "url": "/seasonproject/index.html"
+  },
+  {
+    "revision": "28a6a441306bf723e4e0",
+    "url": "/seasonproject/static/css/main.8bdbbbff.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasonproject/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "28a6a441306bf723e4e0",
+    "url": "/seasonproject/static/js/main.49965c06.chunk.js"
+  },
+  {
+    "revision": "285785276a853f001259",
+    "url": "/seasonproject/static/js/runtime~main.b9b0fe9e.js"
+  }
+]);
